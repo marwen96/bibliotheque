02874c9a1ef614db8283fd03fb37854f671cc3c8
marwen <hellalimarwen96@gmail.com>
2018-12-03 11:08:02 +0100

    auth v 2

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { get } from 'selenium-webdriver/http';
 
 @Injectable({
@@ -10,14 +10,14 @@ import { get } from 'selenium-webdriver/http';
 })
 export class AuthService {
 user$: Observable<firebase.User>;
-  constructor(private afauth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(private afauth: AngularFireAuth, private router: Router) {
     this.user$ = this.afauth.authState ;
    }
 
   login() {
    // tslint:disable-next-line:prefer-const
     this.afauth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-
+    this.router.navigate(['']);
   }
   logout() {
     this.afauth.auth.signOut();
